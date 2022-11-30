8be3d97a458d633598580c1133a6cb4a45a0d6a3
Strangz7 <stephenadebayo79@gmail.com>
2022-11-30 17:11:14 +0100

    Day 3

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Book, Author, Genre, BookInstance, Language
+
+# admin.site.register(Book)
+admin.site.register(Genre)
+# admin.site.register(BookInstance)
+admin.site.register(Language)
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+
+class BookInline(admin.TabularInline):
+    model = Book
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_birth')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        })
+    )
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from django.views import generic
+
+from catalog.models import Book, BookInstance, Author, Genre
+
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_genres = Genre.objects.all().count()
+    num_books_available = Book.objects.filter(bookinstance__status__exact='A').count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {'num_books': num_books,
+               'num_instances': num_instances,
+               'num_instances_available': num_instances_available,
+               'num_authors': num_authors,
+               'num_visits': num_visits,
+               }
+    return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+    context_object_name = 'book_list'
+    # queryset = Book.objects.filter(title__icontains='war')[:5]
+    template_name = 'catalog/book_list.html'
+
+    def get_queryset(self):
+        return Book.objects.filter(title__icontains='life')[:5]
+
+    def get_context_data(self, **kwargs):
+        context = super(BookListView, self).get_context_data(**kwargs)
+        context['some_data'] = 'This is just some data'
+        return context
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+    def book_detail_view(self, request, primary_key):
+        book = get_object_or_404(Book, pk=primary_key)
+        return render(request, 'catalog/book_detail.html', context={'book': book})
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+    context_object_name = 'author_list'
+    template_name = 'catalog/author_list.html'
+
+    def get_queryset(self):
+        return Author.objects.filter(last_name__istartswith='s')[:5]
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+    def book_detail_view(self, request, primary_key):
+        author = get_object_or_404(Author, pk=primary_key)
+        return render(request, 'catalog/author_detail.html', context={'author': author})
